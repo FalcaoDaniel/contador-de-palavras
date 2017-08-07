645138a31989e141f35f2983f2b3e02272484307
FalcaoDaniel <falcao.daniel@gmail.com>
2017-08-07 18:11:04 -0300

    Versão inicial de teste

--- src/main.c
+++ src/main.c
@@ -7,16 +7,93 @@
 
 #include <stdio.h>
 
-int main() {
+int main()
+{
+    char c = 0;
 
-  char c;
+    int words = 0; // contagem de palavras
 
-  c = 1;
+    int lt,cm, pt, num, sep;  // flags atuais de letras (lt), virgula (cm), ponto (pt), numero (num) e separadores (sep, espaço, - , !, ?)
 
-  while (c != '\n') {
-    scanf("%c", &c);
-  }
+    lt = 0; cm = 0; pt = 0; num = 0; sep = 0;
 
-  printf("5\n");
-  return 0;
+    int Alt, Acm, Apt, Anum, Asep;  // flags ANTERIORES de letras (lt), virgula (cm), ponto (pt), numero (num) e separadores (sep, espaço, -, :, !,)
+
+    Alt = 0; Acm = 0; Apt = 0; Anum = 0; Asep = 0;
+
+
+    while (c != '\n')
+    {
+        scanf("%c", &c);
+
+        //Tratamento da entrada
+
+        //Caso 1: Letras
+        if( ((65 <= c && c <= 90) || (97 <= c && c <= 122)) ) {lt = 1;}
+
+        //Caso 2: virgula
+        if(c == 44) {cm = 1;}
+
+        //Caso 3: ponto
+        if(c == 46){pt = 1;}
+
+        //Caso 4: números
+        if(48 <= c && c <= 57){num = 1;}
+
+        //Caso 5: separadores
+        if(c == 32 || c == 33 || c == 45 || c == 58){sep = 1;}
+
+
+        //Significado da entrada
+
+
+
+        if(!sep) //executa sempre que nao for um separador
+        {
+            //Sequencia de letras e números
+            //if((lt==1 || num==1) && (Alt==0 && Anum==0) && Acm==0 && Apt==0 &&  ) {words++;}//Verifica se a letra/número atual faz parte da palavra atual ou se é nova palavra
+
+            //printf("lt: %d\nnum: %d\nAlt: %d\nAnum: %d\n", lt, num, Alt, Anum);
+
+            //Sequencia de letras
+            if(lt==1 && Alt==0) {words++;} //Verifica se a letra atual faz parte da palavra atual ou se é nova palavra
+
+
+            if(num==1 && Anum==0 && Asep==1) {words++;}
+
+            //numeros decimais
+            if( num==1 && Asep==0 && Anum==0 && Acm==0 && Apt==0) {words++;}
+                //somente atualiza words se esse número nao tiver sido precedido
+                //por um separador, número, virgula ou ponto.
+
+/*
+            printf("\nCaractere: %c\n", c);
+
+            printf("\nsep: %d \nnum: %d \ncm: %d \npt: %d \nlt: %d \n ", sep, num, cm, pt, lt);
+
+            printf("\nAsep: %d \nAnum: %d \nAcm: %d \nApt: %d \nAlt: %d \n ", Asep, Anum, Acm, Apt, Alt);
+            printf("\nwords: %d\n", words);
+
+*/
+
+        }
+
+
+        //Copio o estado atual no estado passado
+        Alt  = lt;
+        Acm  = cm;
+        Apt  = pt;
+        Anum = num;
+        Asep = sep;
+
+        //Reseto as flags
+        lt = 0; cm = 0; pt = 0; num = 0; sep = 0;
+
+    }
+
+    printf("%d\n", words);
+
+    return 0;
+
 }
+
